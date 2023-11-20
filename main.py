27fd080811c14ca07235522953ca4c28e4f060bf
Jimmy Lamont <res1mp7q@users.noreply.github.com>
2023-11-20 13:44:52 -0800

    Post the score no matter what, since the lower score is what we are likely monitoring for

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # Excelsior Wireless Ltd Jimmy Lamont 2023
 # A simple NTP Pool dash scraper to publish results to MQTT
 
+from bs4 import BeautifulSoup
 import requests
 import paho.mqtt.client as mqtt
-from bs4 import BeautifulSoup
-url = "https://www.ntppool.org/scores/50.208.57.188"
+
+# URL of the NTP score page
+url = "https://www.ntppool.org/scores/yourIPhere"
+
+# Fetch the HTML content
 response = requests.get(url)
 html_content = response.text
 
+# Parse the HTML
 soup = BeautifulSoup(html_content, "html.parser")
 score_line = soup.find(string="Current score:")
 
 # Define current_score with a default value
 current_score = None
 
+# Extract and print the current score
 if score_line:
     current_score_str = score_line.find_next('span').text.strip()
     try:
@@ -21,27 +27,19 @@ if score_line:
         print(f"Current NTP score: {current_score}")
     except ValueError:
         print(f"Error converting '{current_score_str}' to float.")
-
-# Now you can proceed to the MQTT publishing part if needed.
-if current_score is not None and current_score > 10.0:
-    mqtt_broker = "your_mqtt_broker_address"
-    mqtt_topic = "your_mqtt_topic"
-
-    client = mqtt.Client()
-    client.connect(mqtt_broker, 1883, 60)
-    client.publish(mqtt_topic, f"Current NTP score: {current_score}")
-    client.disconnect()
-elif current_score is None:
-    print("Current score not found on the page.")
 else:
-    print("Current score is not greater than 10.0.")
+    print("Current score not found on the page.")
 
+# MQTT configuration
+mqtt_broker = "mqtt.excelsior.lan"
+mqtt_topic = "ntp/score/state"
 
-if current_score > 10.0:
-    mqtt_broker = "mqtt.excelsior.lan"
-    mqtt_topic = "ntp/score/state"
-    client.username_pw_set("mqtt1", "replaceme")
-    client = mqtt.Client()
-    client.connect(mqtt_broker, 1883, 60)
-    client.publish(mqtt_topic, f"{current_score}")
-    client.disconnect ()
+# Publish the NTP score to MQTT
+client = mqtt.Client()
+client.username_pw_set("mqtt-username", "replaceme")
+client.connect(mqtt_broker, 1883, 60)
+
+# Always publish the score to MQTT, regardless of its value
+client.publish(mqtt_topic, f"{current_score}" if current_score is not None else "Score not available")
+
+client.disconnect()
